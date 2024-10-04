fddbf076c5f84574bae2f063c5b61569a8f3e38f
Nick Stockton <nstockton@users.noreply.github.com>
2024-10-04 08:16:22 -0700

    Use Knickknacks for getting data directory.

--- src/otp_vault/utils.py
+++ src/otp_vault/utils.py
@@ -7,68 +7,13 @@
 from __future__ import annotations
 
 # Built-in Modules:
-import _imp
 import os
-import sys
-from typing import Union
+
+# Third-party Modules:
+from knickknacks.platforms import getDirectoryPath, isFrozen
 
 
 DATA_DIRECTORY: str = "otp_vault_data"
-
-
-def get_freezer() -> Union[str, None]:
-	"""
-	Determines the name of the library used to freeze the code.
-
-	Note:
-		https://github.com/blackmagicgirl/ktools/blob/master/ktools/utils.py
-
-	Returns:
-		The name of the library or None.
-	"""
-	frozen: Union[str, bool, None] = getattr(sys, "frozen", None)
-	if frozen and hasattr(sys, "_MEIPASS"):
-		return "pyinstaller"
-	if frozen is True:
-		return "cx_freeze"
-	if frozen in ("windows_exe", "console_exe", "dll"):
-		return "py2exe"
-	if frozen == "macosx_app":
-		return "py2app"
-	if hasattr(sys, "importers"):
-		return "old_py2exe"
-	if _imp.is_frozen("__main__"):
-		return "tools/freeze"
-	if isinstance(frozen, str):
-		return f"unknown {frozen}"
-	return None
-
-
-def is_frozen() -> bool:
-	"""
-	Determines whether the program is running from a frozen copy or from source.
-
-	Returns:
-		True if frozen, False otherwise.
-	"""
-	return bool(get_freezer())
-
-
-def get_directory_path(*args: str) -> str:
-	"""
-	Retrieves the path of the directory where the program is located.
-
-	Args:
-		*args: Positional arguments to be passed to os.join after the directory path.
-
-	Returns:
-		The path.
-	"""
-	if is_frozen():
-		path = os.path.dirname(sys.executable)
-	else:
-		path = os.path.join(os.path.dirname(__file__), os.path.pardir)
-	return os.path.realpath(os.path.join(path, *args))
 
 
 def get_data_path(*args: str) -> str:
@@ -81,4 +26,5 @@ def get_data_path(*args: str) -> str:
 	Returns:
 		The path.
 	"""
-	return os.path.realpath(os.path.join(get_directory_path(DATA_DIRECTORY), *args))
+	path: str = getDirectoryPath(os.path.curdir if isFrozen() else os.path.pardir, DATA_DIRECTORY)
+	return os.path.realpath(os.path.join(path, *args))
--- tests/otp_vault/test_utils.py
+++ tests/otp_vault/test_utils.py
@@ -8,58 +8,16 @@ from __future__ import annotations
 
 # Built-in Modules:
 import os
-import sys
 from unittest import TestCase
-from unittest.mock import Mock, patch
 
 # OTP Vault Modules:
 from otp_vault import utils
 
 
 class TestUtils(TestCase):
-	@patch("otp_vault.utils._imp")
-	@patch("otp_vault.utils.sys")
-	def test_get_freezer(self, mock_sys: Mock, mock_imp: Mock) -> None:
-		del mock_sys.frozen
-		del mock_sys._MEIPASS
-		del mock_sys.importers
-		mock_imp.is_frozen.return_value = True
-		self.assertEqual(utils.get_freezer(), "tools/freeze")
-		mock_imp.is_frozen.return_value = False
-		self.assertIs(utils.get_freezer(), None)
-		mock_sys.importers = True
-		self.assertEqual(utils.get_freezer(), "old_py2exe")
-		del mock_sys.importers
-		for item in ("windows_exe", "console_exe", "dll"):
-			mock_sys.frozen = item
-			self.assertEqual(utils.get_freezer(), "py2exe")
-		mock_sys.frozen = "macosx_app"
-		self.assertEqual(utils.get_freezer(), "py2app")
-		mock_sys.frozen = True
-		self.assertEqual(utils.get_freezer(), "cx_freeze")
-		mock_sys.frozen = "some undefined freezer"
-		self.assertEqual(utils.get_freezer(), "unknown some undefined freezer")
-		mock_sys._MEIPASS = "."
-		self.assertEqual(utils.get_freezer(), "pyinstaller")
-
-	def test_is_frozen(self) -> None:
-		self.assertIs(utils.is_frozen(), False)
-
-	@patch("otp_vault.utils.is_frozen")
-	def test_get_directory_path(self, mock_is_frozen: Mock) -> None:
-		subdirectory: tuple[str, ...] = ("level1", "level2")
-		frozen_dir_name: str = os.path.dirname(sys.executable)
-		frozen_output: str = os.path.realpath(os.path.join(frozen_dir_name, *subdirectory))
-		mock_is_frozen.return_value = True
-		self.assertEqual(utils.get_directory_path(*subdirectory), frozen_output)
-		unfrozen_dir_name: str = os.path.join(os.path.dirname(utils.__file__), os.path.pardir)
-		unfrozen_output: str = os.path.realpath(os.path.join(unfrozen_dir_name, *subdirectory))
-		mock_is_frozen.return_value = False
-		self.assertEqual(utils.get_directory_path(*subdirectory), unfrozen_output)
-
 	def test_get_data_path(self) -> None:
 		subdirectory: tuple[str, ...] = ("level1", "level2")
-		output: str = os.path.realpath(
-			os.path.join(utils.get_directory_path(utils.DATA_DIRECTORY), *subdirectory)
+		output: str = os.path.join(
+			os.path.dirname(utils.__file__), os.path.pardir, utils.DATA_DIRECTORY, *subdirectory
 		)
-		self.assertEqual(utils.get_data_path(*subdirectory), output)
+		self.assertEqual(utils.get_data_path(*subdirectory), os.path.realpath(output))
